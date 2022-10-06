c50cdc2b6e01e4971077967a41b90c45eb4b6daf
Andrew7567 <andrei.petrukhin@yandex.ru>
2022-10-06 18:01:43 +0300

    isort

--- api_yamdb/api_yamdb/reviews/admin.py
+++ api_yamdb/api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
-from .models import Category, Genre, Title, GenreTitle, Review, Comment
-
 from django.contrib import admin
+from .models import (Category, Genre, Title, GenreTitle, Review, Comment,
+                     User)
 
 admin.site.register(Category)
 admin.site.register(Genre)
@@ -8,3 +8,4 @@ admin.site.register(Title)
 admin.site.register(GenreTitle)
 admin.site.register(Review)
 admin.site.register(Comment)
+admin.site.register(User)
