38a5b137d1b8fd87adb23a29a8a85e3164e711a0
Andrew7567 <andrei.petrukhin@yandex.ru>
2022-10-06 18:03:40 +0300

    new commit

--- api_yamdb/api_yamdb/reviews/admin.py
+++ api_yamdb/api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import (Category, Genre, Title, GenreTitle, Review, Comment,
-                     User)
+
+from .models import Category, Genre, Title, GenreTitle, Review, Comment, User
 
 admin.site.register(Category)
 admin.site.register(Genre)
