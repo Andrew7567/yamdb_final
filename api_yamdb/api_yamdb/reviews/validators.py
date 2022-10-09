9820f704f43475e8e24faeab28942fdfd30af2e3
Andrew7567 <andrei.petrukhin@yandex.ru>
2022-10-09 13:30:03 +0300

    structure updated

--- api_yamdb/api_yamdb/reviews/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.validators import ValidationError
-from django.utils import timezone
-
-
-def validate_year(value):
-    if value > timezone.now().year:
-        raise ValidationError(
-            (f'Год {value} больше текущнго года!')
-        )
