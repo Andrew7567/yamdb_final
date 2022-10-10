90c8d02a568a5973bda2767991c8b6deb4a8e8fa
Andrew7567 <andrei.petrukhin@yandex.ru>
2022-10-10 22:13:28 +0300

    flake8

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Category, Comment, Genre, GenreTitle, Review, Title
+
 admin.site.register(Category)
 admin.site.register(Genre)
 admin.site.register(Title)
